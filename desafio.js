a3d7fa6ac91d1bad1938a0bb9fd24298d2275b0c
wwpedro <pedrinhosilva35997@gmail.com>
2023-11-23 13:22:43 -0300

    desafio finalizado

--- desafio.js
+++ desafio.js
@@ -1,28 +1,62 @@
-function somatorioNumDivisivel(){
-    
-    let numero = prompt("Um numero:");
-    let somatorio = 0;
-    if(numero > 0){
+function realizarSomatorioNumerosDivisiveis3e5() {
 
-        
-        for (let i = 1; i < numero; i++) {
-            if (i%3 == 0 || i%5 == 0){
-                somatorio=somatorio+i
+    Swal.fire({
+        title: 'Digite um número:',
+        input: 'text',
+        inputPlaceholder: 'Digite aqui',
+        showCancelButton: false,
+        confirmButtonText: 'OK',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            let numero = result.value;
+
+            let somatorio = 0;
+
+            if (isNaN(numero)) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Digite um valor numérico!',
+                }).then(() => {
+                    realizarSomatorioNumerosDivisiveis3e5();
+                });
+            } else {
+                if (numero > 0) {
+                    for (let i = 1; i < numero; i++) {
+                        if (i % 3 == 0 || i % 5 == 0) {
+                            somatorio = somatorio + i;
+                        }
+                    }
+                    if (somatorio == 0) {
+                        Swal.fire({
+                            icon: 'info',
+                            title: 'Atenção',
+                            text: 'O somatório dos números divisíveis por 3 e 5 até igual a 0, pois você digitou números abaixo de 3.',
+                        }).then(() => {
+                            realizarSomatorioNumerosDivisiveis3e5();
+                        });
+                    } else {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Sucesso',
+                            text: 'O somatório dos números divisíveis por 3 e 5 até o número que você digitou é ' + somatorio,
+                        }).then(() => {
+                            realizarSomatorioNumerosDivisiveis3e5();
+                        });
+                    }
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Digite um número positivo maior que 0!',
+                    }).then(() => {
+                        realizarSomatorioNumerosDivisiveis3e5();
+                    });
+                }
             }
-                
         }
-        if(somatorio==0){
-            alert("o somatorio dos numero divisiveis por 3 e 5 até igual a 0 pois voce digitou numeros abaixo de 3")
-            somatorioNumDivisivel();
-        }else{
-            alert("o somatorio dos numero divisiveis por 3 e 5 até o numero que voce digitou eh "+somatorio);
-            somatorioNumDivisivel();
-        }
+    });
 
-    }else{
-        alert("digite um numero positivo maior que 0")
-        somatorioNumDivisivel();
-    }
 }
 
-somatorioNumDivisivel();
+realizarSomatorioNumerosDivisiveis3e5();
