0d873389c8be2a7e29f0abf6f63543316ce522e2
wwpedro <pedrinhosilva35997@gmail.com>
2023-11-23 13:34:28 -0300

    desafio finalizado

--- desafio.js
+++ desafio.js
@@ -6,6 +6,7 @@ function realizarSomatorioNumerosDivisiveis3e5() {
         inputPlaceholder: 'Digite aqui',
         showCancelButton: false,
         confirmButtonText: 'OK',
+        allowOutsideClick: false,
     }).then((result) => {
         if (result.isConfirmed) {
             let numero = result.value;
@@ -17,6 +18,7 @@ function realizarSomatorioNumerosDivisiveis3e5() {
                     icon: 'error',
                     title: 'Oops...',
                     text: 'Digite um valor numérico!',
+                    allowOutsideClick: false,
                 }).then(() => {
                     realizarSomatorioNumerosDivisiveis3e5();
                 });
@@ -32,6 +34,7 @@ function realizarSomatorioNumerosDivisiveis3e5() {
                             icon: 'info',
                             title: 'Atenção',
                             text: 'O somatório dos números divisíveis por 3 e 5 até igual a 0, pois você digitou números abaixo de 3.',
+                            allowOutsideClick: false,
                         }).then(() => {
                             realizarSomatorioNumerosDivisiveis3e5();
                         });
@@ -40,6 +43,7 @@ function realizarSomatorioNumerosDivisiveis3e5() {
                             icon: 'success',
                             title: 'Sucesso',
                             text: 'O somatório dos números divisíveis por 3 e 5 até o número que você digitou é ' + somatorio,
+                            allowOutsideClick: false,
                         }).then(() => {
                             realizarSomatorioNumerosDivisiveis3e5();
                         });
@@ -49,6 +53,7 @@ function realizarSomatorioNumerosDivisiveis3e5() {
                         icon: 'error',
                         title: 'Oops...',
                         text: 'Digite um número positivo maior que 0!',
+                        allowOutsideClick: false,
                     }).then(() => {
                         realizarSomatorioNumerosDivisiveis3e5();
                     });
